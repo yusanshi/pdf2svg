5887d032c7e7561072142e06f5efef8b8c30e480
yusanshi <yulei2018@gmail.com>
2020-03-30 18:15:14 +0800

    Simplify contents

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-pdf2svg
-=======
+# pdf2svg
 
-A PDF to SVG converter written using PDF.js 
+A PDF to SVG converter written using PDF.js.
+
+## Usage
+
+`node pdf2svg.js pdfPath [outputPath]`, `outputPath` will be `./` if not provided.
+
+It will generate `originalName.svg` for a single page PDF file, `originalName-page.svg` for a multi-page PDF file.
+
+## Make it available globally
+
+```bash
+npm link
+```
+Then you can run `pdf2svg pdfPath [outputPath]` globally.
+
+See <https://blog.npmjs.org/post/118810260230/building-a-simple-command-line-tool-with-npm> and <https://medium.com/netscape/a-guide-to-create-a-nodejs-command-line-package-c2166ad0452e>.
+
+> If you are on Linux, to avoid conflict with pdf2svg in package manager, you may want to specify a different name for this package. To do this, modify `pdf2svg` in `"bin": {"pdf2svg": "pdf2svg.js"}` in `package.json` to another name before running `npm link`.
+
--- pdf2svg.js
+++ pdf2svg.js
@@ -1,3 +1,8 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-console */
+/* eslint-disable no-use-before-define */
+
 /* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
 /* Any copyright is dedicated to the Public Domain.
@@ -5,111 +10,83 @@
 
 //
 // Node tool to dump SVG output into a file.
+// Usage: node pdf2svg.js pdfPath [outputPath]
+// outputPath will be './' if not provided
 //
 
-var fs = require('fs');
+const fs = require('fs');
+const path = require('path');
 
 // HACK few hacks to let PDF.js be loaded not as a module in global space.
 global.window = global;
 global.navigator = { userAgent: 'node' };
 global.PDFJS = {};
 
-PDFJS.workerSrc = true;
 require('./js/pdf.combined.js');
 require('./js/domstubs.js');
 
-
 // Loading file from file system into typed array
-var pdfPath = process.argv[2] || 'tracemonkey.pdf';
-var data = new Uint8Array(fs.readFileSync(pdfPath));
+const pdfPath = process.argv[2];
+if (pdfPath === undefined) {
+  throw new Error('PDF path not provided.');
+}
+const outputPath = process.argv[3] || './';
 
-// Dumps svg outputs to a folder called svgdump
-function writeToFile(svgdump, pageNum) {
-  var name = getFileNameFromPath(pdfPath);
-  fs.mkdir('./svgdump/', function(err) {
+const data = new Uint8Array(fs.readFileSync(pdfPath));
+
+// Dumps svg outputs to outputPath
+function writeToFile(svgdump, pageNum, isSinglePage) {
+  fs.mkdir(outputPath, { recursive: true }, (err) => {
     if (!err || err.code === 'EEXIST') {
-      fs.writeFile('./svgdump/' + name + "-" + pageNum + '.svg', svgdump,
-        function(err) {
-          if (err) {
-            console.log('Error: ' + err);
-          } else {
-            console.log('Page: ' + pageNum);
+      fs.writeFile(
+        path.join(outputPath, `${path.basename(pdfPath, path.extname(pdfPath))}${isSinglePage ? '' : `-${pageNum}`}.svg`),
+        svgdump,
+        (err2) => {
+          if (err2) {
+            console.error(`Error: ${err2}`);
           }
-        });
+        },
+      );
     }
   });
-}
-
-function createIndex(pdfPath, numPages) {
-  var name = getFileNameFromPath(pdfPath);
-  var page= 1;
-
-  // Read from a template file
-  var html = fs.readFileSync('./template.html');
-  var htmlend = '</div></div></div></body></html>';
-
-  fs.writeFile('./index.html', html, function (err) {
-    if (err) {
-      console.log("Error");
-    }
-  });
-  
-  while (page <= numPages) {
-    var attr = 'style="background-image:url(./svgdump/' + name + '-' + page++ + '.svg);"';
-    var div = '<div class="sheet"' + attr + '></div>\n'
-    fs.appendFile('./index.html', div, function(err) {
-      if (err) {
-        console.log("Error");
-      }
-    });
-  }
-  fs.appendFile('./index.html', htmlend, function (err) {
-    if (err) {
-      console.log("Error creating index file");
-    }
-  });
-}
-
-// Get filename from the path
-function getFileNameFromPath(path) {
-  var index = path.lastIndexOf('/');
-  var extIndex = path.lastIndexOf('.');
-  return path.substring(index , extIndex);
 }
 
 // Will be using promises to load document, pages and misc data instead of
 // callback.
-PDFJS.getDocument(data).then(function (doc) {
-  var numPages = doc.numPages;
-  console.log('# Document Loaded');
-  console.log('Number of Pages: ' + numPages);
-  console.log();
+// eslint-disable-next-line no-undef
+PDFJS.getDocument(data)
+  .then((doc) => {
+    const { numPages } = doc;
+    console.log('# Document Loaded');
+    console.log(`Number of Pages: ${numPages}`);
+    console.log();
 
-  createIndex(pdfPath, numPages);
-  var lastPromise = Promise.resolve(); // will be used to chain promises
-  var loadPage = function (pageNum) {
-    return doc.getPage(pageNum).then(function (page) {
-      var viewport = page.getViewport(1.0 /* scale */);
-      console.log();
-      
-      return page.getOperatorList().then(function (opList) {
-        var svgGfx = new PDFJS.SVGGraphics(page.commonObjs, page.objs);
+    let lastPromise = Promise.resolve(); // will be used to chain promises
+    const loadPage = (pageNum) => doc.getPage(pageNum).then((page) => {
+      console.log(`Current page: ${pageNum}`);
+      const viewport = page.getViewport(1.0);
+      return page.getOperatorList().then((opList) => {
+        // eslint-disable-next-line no-undef
+        const svgGfx = new PDFJS.SVGGraphics(page.commonObjs, page.objs);
         svgGfx.embedFonts = true;
-        return svgGfx.getSVG(opList, viewport).then(function (svg) {
-          var svgDump = svg.toString();
-          writeToFile(svgDump, pageNum);
+        return svgGfx.getSVG(opList, viewport).then((svg) => {
+          const svgDump = svg.toString();
+          writeToFile(svgDump, pageNum, numPages === 1);
+          console.log();
         });
       });
-    })
-  };
-  
-  for (var i = 1; i <= numPages; i++) {
-    lastPromise = lastPromise.then(loadPage.bind(null, i));
-  }
-  return lastPromise;
-}).then(function () {
-  console.log('# End of Document');
-}, function (err) {
-  console.error('Error: ' + err);
-});
+    });
 
+    for (let i = 1; i <= numPages; i += 1) {
+      lastPromise = lastPromise.then(loadPage.bind(null, i));
+    }
+    return lastPromise;
+  })
+  .then(
+    () => {
+      console.log('# End of Document');
+    },
+    (err) => {
+      console.error(`Error: ${err}`);
+    },
+  );
